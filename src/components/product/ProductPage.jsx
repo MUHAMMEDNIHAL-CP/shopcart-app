bbc71159b5b2137509bf62d5fb5f003daf04e567
Nihal <muhdnihal3200@gmail.com>
2025-01-18 13:18:06 +0530

    new commited

--- src/components/product/ProductPage.jsx
+++ src/components/product/ProductPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import ProductPagePlaceholder from "./ProductPagePlaceholder.tsx";
+import ProductPagePlaceHolder from "./ProductPagePlaceHolder"
 import RelatedProducts from "./RelatedProducts"
 import { useEffect, useState } from "react"
 import { BASE_URL } from "../../api"
