b4c1e334c93c553c13da2961447630ff435391be
Nihal <muhdnihal3200@gmail.com>
2025-02-09 21:49:29 +0530

    header modified

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const banners = [
+  {
+    image: "https://rukminim1.flixcart.com/fk-p-flap/1620/270/image/766ea612e03ff01d.jpg?q=20", // Replace with your image URL
+  },
+  {
+    image: "https://rukminim1.flixcart.com/fk-p-flap/1620/270/image/3f976ca32ddc651e.jpg?q=20",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/fk-p-flap/1620/270/image/c928b14a5cddaf18.jpg?q=20",
+  },
+];
 
 const Header = () => {
-    return (
-        <header className="py-5" style={{ backgroundColor: "#6050DC", marginTop: '65px' }}>
-            <div className="container px-4 px-lg-5 my-5">
-                <div className="text-center text-white">
-                    <h1 className="display-4 fw-bold">Welcome to Your Favorite Store</h1>
-                    <p className="lead fw-normal text-white-75 mb-4">Discover the latest trends with our modern collection</p>
-                    <a href="#shop" className="btn btn-light btn-lg rounded-pill px-4 py-2">Shop Now</a>
-                </div>
+  return (
+    <div className="banner-slider">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop
+        className="swiper-container"
+      >
+        {banners.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <a to="/#shop"> 
+            <div className="banner">
+              <img src={banner.image} className="banner-image" />
             </div>
-        </header>
-    )
-}
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
